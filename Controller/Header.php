25969fa7628616f93a855ef7ccdbcb45acd6f03a
Your Name <you@example.com>
2021-12-12 17:08:23 +0600

    final

--- Controller/Header.php
+++
@@ -1,9 +0,0 @@
-<?php 
-		session_start();
-		if(!isset($_COOKIE['flag']))
-		{
-			header('location: ../View/Login.php');
-		}
-	
-
-?>
--- Index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	header('location: View/Login.php');
-
-?>
